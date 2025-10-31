65c410faae449761c6d207b041648e77805a80e1
BED-COM-32-20 <kelvinmponda47@gmail.com>
2025-10-31 10:48:36 +0200

    Added a footer to the new pages

--- src/app/pages/privacy/page.jsx
+++ src/app/pages/privacy/page.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import Link from "next/link"
 import Header from "@/components/layout/Header"
+import Footer from "@/components/layout/Footer"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
@@ -12,6 +13,7 @@ export default function PrivacyPage() {
         <div className="min-h-screen bg-background">
             <Header />
             This is the privacy page
+            <Footer />
         </div>
     )
 }
--- src/app/pages/terms/page.jsx
+++ src/app/pages/terms/page.jsx
@@ -3,4 +3,17 @@
 import { useState } from "react"
 import Link from "next/link"
 import Header from "@/components/layout/Header"
+import Footer from "@/components/layout/Footer"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function TermsPage() {
+
+    return (
+        <div className="min-h-screen bg-background">
+            <Header />
+            This is the terms page
+            <Footer />
+        </div>
+    )
+}
